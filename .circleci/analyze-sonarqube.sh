ff4a05d8497e03f281cf9498110ff4e7d4754c43
mouse <a.4card@gmail.com>
2018-05-24 01:20:52 +0900

    Sonar連携追加(修正)

--- .circleci/analyze-sonarqube.sh
+++ .circleci/analyze-sonarqube.sh
@@ -5,6 +5,7 @@ SONAR_DIR="sonar-scanner-3.0.3.778"
 
 wget -P $HOME -N "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/${SONAR_VERSION}.zip"
 unzip -d $HOME $HOME/$SONAR_VERSION.zip
+PR_NUMBER=`echo "${CI_PULL_REQUEST}" | sed -e 's/.*\///g'`
 
 SONAR_PARAMS="-Dsonar.projectKey=$CIRCLE_PROJECT_REPONAME \
             -Dsonar.login=$SONAR_TOKEN \
@@ -18,6 +19,7 @@ SONAR_PARAMS="-Dsonar.projectKey=$CIRCLE_PROJECT_REPONAME \
             -Dsonar.github.endpoint='https://api.github.com' \
             -Dsonar.github.repository=$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME \
             -Dsonar.projectKey=$SONAR_PJKEY \
-            -Dsonar.analysis.mode=issues "
+            -Dsonar.github.pullRequest=${PR_NUMBER} \
+            -Dsonar.analysis.mode=preview "
 
 $HOME/$SONAR_DIR/bin/sonar-scanner $SONAR_PARAMS
